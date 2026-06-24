81c4ab6b3a3ac166a8f5d8daeb71a8eadf39236d
mvadim556-hue <mvadim556@gmail.com>
2026-06-24 15:02:02 +0700

    Create MainWindow.xaml.cs

---
+++ GDCCwpf/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace GDCCwpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
