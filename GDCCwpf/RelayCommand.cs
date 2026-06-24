685a73b33ed41b1ec80e2f7a93700b354fad4a15
mvadim556-hue <mvadim556@gmail.com>
2026-06-24 15:02:56 +0700

    Create RelayCommand.cs

---
+++ GDCCwpf/RelayCommand.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Windows.Input;
+
+namespace GDCCwpf
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter) => true;
+
+        public void Execute(object parameter) => _execute(parameter);
+    }
+}
